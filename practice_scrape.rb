6a799dd0657d0d6454cad361eb8b2b0e84799fd1
415DomSmith <415.dominic.smith@gmail.com>
2015-08-04 12:17:20 -0700

    tweaked practice scraper

--- practice_scrape.rb
+++ practice_scrape.rb
@@ -23,14 +23,46 @@ require 'rest-client'
 	# p links
 
 
- 	url = 'http://sf.funcheap.com/de-young-museum-free-admission-day-first-tuesdays-24/'
+ 	# url = 'http://sf.funcheap.com/free-admission-day-museum-of-craft-and-design-dogpatch-22/' #GOOD
+ 	# url = 'http://sf.funcheap.com/legion-of-honor-free-admission-day-first-tuesdays-24/' #GOOD
+ 	# url = 'http://sf.funcheap.com/annual-san-francisco-jewish-film-festival-free-movie-day-sf/' #GOOD
+ 	# url = 'http://sf.funcheap.com/2015-backtoschool-backpack-giveaway-sf/' #IMG DOESN'T WORK
+ 	# url = 'http://sf.funcheap.com/neuroscience-love-palo-alto/' #IMG DOESN'T WORK
+ 	# url = 'http://sf.funcheap.com/life-literary-fiction-book-conversation-sf/' #ONLY GETS IMG (gif)
+ 	# url = 'http://sf.funcheap.com/summertime-outdoor-pop-rock-concert-orinda/' #GOOD
+ 	# url = 'http://sf.funcheap.com/its-just-2-minutes-stand-up-comedy-game-show-haight-13/' #GOOD
+ 	# url = 'http://sf.funcheap.com/hot-flash-heat-wave-garage-pop-rickshaw-stop/' #IMG DOESN'T WORK
+ 	# url = 'http://sf.funcheap.com/summer-stride-2015-visions-loveliness-book-event-sf/' #IMG DOESN'T WORK
+ 	# url = 'http://sf.funcheap.com/people-plazas-free-rb-concert-sf/' #GOOD
+ 	# url = 'http://sf.funcheap.com/oakland-baltimore-orioles-2-ticket-day-2/' #GOOD
+ 	# url = 'http://sf.funcheap.com/drink-draw-art-event-raffle-the-knockout-14/' #GOOD
+
 
  	response = RestClient.get url, :user_agent => 'Chrome'
  	page = Nokogiri::HTML(response)
 
- 	title = page.css('div.post h1.title').text
- 	date = page.css('div#stats a').first.text
- 	cost = page.css('div.post span.cost + a.tt:not(div.tooltip)').text
- 	time =
+ 	title = page.css('header.entry-header h1.entry-title').text
+ 	date = page.css('div.entry-meta span.date-time a').text
+ 	cost = page.css('div.entry-meta span.cost').text
+ 	time = page.css('div.entry-meta span.date-time > span').text
+ 	city = page.css('div.entry-meta span.region-parent').text
+ 	address = page.css('div.entry-meta span.venue').text
+ 	# image_url = page.css('div.clearfloat img').attr('src').text
+ 	# image_url = page.css('meta og:image').text
+ 	description = page.css('div.entry-content-description p').text
+ 
+
+ 	p title
+ 	p date
+ 	p cost
+ 	p time
+ 	p city
+ 	p address
+ 	# p image_url
+ 	p description
+ 
+ 	# p page.to_s
+
+ 	# p page.to_s
  	# p cost.split('\r')
  	# p content
